1d847276519ebda2be41b3e517b177c4b42c01b6
Dione Wilson <dionew1@gmail.com>
2018-02-18 16:43:34 -0700

    Correct the seed file for 1 return.

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,22 +1,21 @@
 
 exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE foods CASCADE')
-  return knex('foods').del()
-    .then(function () {
-      return Promis.all([
-        knex('foods').insert([
-          { name: "Vegetables" },
-          { name: "Roasted Vegetables" },
-          { name: "Soft Cheese" },
-          { name: "Hard Cheese" },
-          { name: "Starches" },
-          { name: "Fish" },
-          { name: "Rich Fish" },
-          { name: "White Meat" },
-          { name: "Red Meat" },
-          { name: "Cured Meat" },
-          { name: "Sweets" },
-        ])
+  .then(function () {
+    return Promise.all([
+      knex('foods').insert([
+        { name: "Vegetables" },
+        { name: "Roasted Vegetables" },
+        { name: "Soft Cheese" },
+        { name: "Hard Cheese" },
+        { name: "Starches" },
+        { name: "Fish" },
+        { name: "Rich Fish" },
+        { name: "White Meat" },
+        { name: "Red Meat" },
+        { name: "Cured Meat" },
+        { name: "Sweets" },
       ])
-    })
+    ])
+  })
 }
