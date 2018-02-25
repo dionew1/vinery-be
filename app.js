4f5525f5f9f5f0d47d440773c1918b1666cda0d0
Dione Wilson <dionew1@gmail.com>
2018-02-24 20:40:55 -0700

    Add cors and whitelist sites.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,6 +17,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(cors({
+  origin: ['https://vinery-fe.herokuapp.com/', 'http://localhost:3000']
+}));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
